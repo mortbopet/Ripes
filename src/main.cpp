3b884cf1e72366f9bd29f126bbdabedade082774
Morten Borup Petersen <morten_bp@live.dk>
2017-10-30 21:10:39 +0100

    Add argc/argv inputs to main function

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,15 @@
 
 using namespace std;
 
-int main(/*argv, argc*/) {
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    // insert error msg
+    return 1;
+  }
 
-  cout << "Hello World!" << endl;
-
-  char *ex;
-  Parser parser(ex);
+  Parser parser(argv[1]);
   Runner runner(&parser);
 
+  // execute runner
   return runner.exec();
 }
