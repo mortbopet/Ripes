961289c03e58ac636f214f72e8236bb788129253
jacobruager <s163957@student.dtu.dk>
2017-11-01 23:03:06 +0100

    Work on instruction file parse functionality

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,7 +1,10 @@
 #include "parser.h"
+#include <fstream>
 
 Parser::Parser(char *fileName) {}
 Parser::~Parser() {}
+
+uint32_t Parser::readWord();
 
 bool Parser::parseInstruction(int pc) {
   // Given a program counter, parse instruction from binary file. Return false
@@ -9,7 +12,23 @@ bool Parser::parseInstruction(int pc) {
 
   // m_currentInstruction = ...
 
-  return true;
+  // For first time, ceate object. Check object, and report error if
+  // incorrect file or path
+  if (!binaryFile) {
+    ifstream binaryFile(*fileName, ios::binary);
+  }
+  if (!binaryFile.good()) {
+    return false;
+  }
+
+  // Read file with respect to pc, and put into uint32. If not possible, return
+  // false.
+  if (binaryFile.read(readWord(), pc)) {
+    binaryFile.close();
+    return true;
+  } else {
+    return false;
+  }
 }
 
 instrType Parser::getOpType(uint32_t word) { return INVALID; }
