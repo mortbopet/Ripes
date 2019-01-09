91906284e2587f0c40d817305cea1bf4d1a02188
Morten Borup Petersen <mortbopet@users.noreply.github.com>
2019-01-09 18:40:35 +0100

    Switch Windows CI mxe distribution to mirror
    https://github.com/mxe/mxe/issues/2253 and still down

--- scripts/windows/before_install.sh
+++ scripts/windows/before_install.sh
@@ -2,10 +2,12 @@
 
 sudo apt-get update
 
-echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" \
-    | sudo tee /etc/apt/sources.list.d/mxeapt.list
-sudo apt-key adv --keyserver x-hkp://keys.gnupg.net \
-    --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
+#echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" \
+#    | sudo tee /etc/apt/sources.list.d/mxeapt.list
+#sudo apt-key adv --keyserver x-hkp://keys.gnupg.net \
+#    --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 86B72ED9
+sudo add-apt-repository 'deb [arch=amd64] http://mirror.mxe.cc/repos/apt trusty main'
 
 sudo apt-get update
 
