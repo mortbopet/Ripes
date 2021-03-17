885384e4c6315788821730e2193552f36cb36ff7
Tobias Mueller <mail@tcmueller.de>
2021-03-17 13:13:04 +0100

    Added Test for new riscv processor w/o fw

--- test/tst_riscv.cpp
+++ test/tst_riscv.cpp
@@ -66,6 +66,7 @@ private slots:
 
     void testRVSingleCycle() { runTests(ProcessorID::RVSS); }
     void testRV5StagePipeline() { runTests(ProcessorID::RV5S); }
+    void testRV5StagePipelineNOFW() { runTests(ProcessorID::RV5S_NO_FW); }
 
     void cleanupTestCase();
 };
