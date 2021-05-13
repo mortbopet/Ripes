1cd894fc4277c2e70b09fc81b60b1a2d7c6199eb
Tobias Mueller <mail@tcmueller.de>
2021-05-13 13:31:18 +0200

    Renamed SparseArray to AddressSpace

--- src/processors/RISC-V/rv5s_no_fw/rv5s_no_fw.h
+++ src/processors/RISC-V/rv5s_no_fw/rv5s_no_fw.h
@@ -388,9 +388,9 @@ public:
         propagateDesign();
     }
     void setPCInitialValue(uint32_t address) override { pc_reg->setInitValue(address); }
-    SparseArray& getMemory() override { return *m_memory; }
+    AddressSpace& getMemory() override { return *m_memory; }
     unsigned int getRegister(RegisterFileType rfid, unsigned i) const override { return registerFile->getRegister(i); }
-    SparseArray& getArchRegisters() override { return *m_regMem; }
+    AddressSpace& getArchRegisters() override { return *m_regMem; }
     void finalize(const FinalizeReason& fr) override {
         if (fr.exitSyscall && !ecallChecker->isSysCallExiting()) {
             // An exit system call was executed. Record the cycle of the execution, and enable the ecallChecker's system
