95b09b30c0d0978ca79ed156d00bf6b280b763a0
Morten Borup Petersen <morten_bp@live.dk>
2021-05-21 20:55:59 +0200

    Use uValue instead of templated VSRTL port value function

--- src/processors/RISC-V/rv6s_dual/rv6s_dual_instr_mem.h
+++ src/processors/RISC-V/rv6s_dual/rv6s_dual_instr_mem.h
@@ -14,7 +14,7 @@ class ROM_DUAL : public ROM<addrWidth, dataWidth, byteIndexed> {
 public:
     ROM_DUAL(std::string name, SimComponent* parent) : ROM<addrWidth, dataWidth, byteIndexed>(name, parent) {
         data_out2 << [=] {
-            const auto accessAddress = this->addr.template value<VSRTL_VT_U>() + 4;
+            const auto accessAddress = this->addr.uValue() + 4;
             const auto v = this->read(accessAddress);
             return v;
         };
