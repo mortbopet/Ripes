5d31ec96af6562d374fabca6f76b6c971890584d
Morten Borup Petersen <morten_bp@live.dk>
2021-06-13 10:57:16 +0200

    Allow time-limited status messages

--- src/statusmanager.h
+++ src/statusmanager.h
@@ -2,6 +2,7 @@
 
 #include <QObject>
 #include <QString>
+#include <QTimer>
 
 namespace Ripes {
 class StatusEmitter : public QObject {
@@ -21,18 +22,26 @@ signals:
  * Defines a singleton class which may be used to send or clear a message to a specific label within the main window
  * status bar.
  */
-#define StatusManager(name)                                                     \
-    class name##StatusManager {                                                 \
-    public:                                                                     \
-        static name##StatusManager& get() {                                     \
-            static name##StatusManager s;                                       \
-            return s;                                                           \
-        }                                                                       \
-                                                                                \
-        static void setStatus(const QString& s) { get().emitter.setStatus(s); } \
-        static void clearStatus() { get().emitter.clearStatus(); }              \
-        StatusEmitter emitter;                                                  \
-                                                                                \
-    private:                                                                    \
-        name##StatusManager() {}                                                \
+#define StatusManager(name)                                                               \
+    class name##StatusManager {                                                           \
+    public:                                                                               \
+        static name##StatusManager& get() {                                               \
+            static name##StatusManager s;                                                 \
+            return s;                                                                     \
+        }                                                                                 \
+                                                                                          \
+        static void setStatus(const QString& s) { get().emitter.setStatus(s); }           \
+        static void setStatusTimed(const QString& s, int ms) {                            \
+            setStatus(s);                                                                 \
+            get().m_timer.start(ms);                                                      \
+        }                                                                                 \
+        static void clearStatus() { get().emitter.clearStatus(); }                        \
+        StatusEmitter emitter;                                                            \
+                                                                                          \
+    private:                                                                              \
+        name##StatusManager() {                                                           \
+            m_timer.setSingleShot(true);                                                  \
+            m_timer.connect(&m_timer, &QTimer::timeout, [this] { this->clearStatus(); }); \
+        }                                                                                 \
+        QTimer m_timer;                                                                   \
     };
