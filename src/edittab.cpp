c2130c5769ed573ab2c302f25fe3f85fa2efd0c1
Morten Borup Petersen <morten_bp@live.dk>
2021-07-09 11:04:23 +0200

    Attempt reassemble on processor change

--- src/edittab.cpp
+++ src/edittab.cpp
@@ -221,6 +221,9 @@ void EditTab::onProcessorChanged() {
     // Notify a possible assembler change to the code editor - opcodes might have been added or removed which must be
     // reflected in the syntax highlighter
     m_ui->codeEditor->setSourceType(m_currentSourceType, ProcessorHandler::getAssembler()->getOpcodes());
+
+    // Try reassembling
+    sourceCodeChanged();
 }
 
 void EditTab::sourceCodeChanged() {
