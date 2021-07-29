f0b60daa97e0fb43cdead0a4beeb4695859fc29b
Morten Borup Petersen <morten_bp@live.dk>
2021-07-29 10:50:54 +0200

    Make versioning always show diff from last tag to current commit

--- .github/workflows/scripts/set_version.sh
+++ .github/workflows/scripts/set_version.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 set -e
-if ([ "${GITHUB_REF##*/}" = "master" ]) then
-    export RIPES_VERSION=continuous
-else
-    # get tag name and convert '/' to '-'
-    export RIPES_VERSION=$(echo ${GITHUB_REF#refs/*/} | sed -e 's/\//-/g')
-fi
+# Get the last tag which is not the top-of-head continuous tag
+export RIPES_VERSION=$(git describe --tags --exclude "continuous")
