02a78d876af21e460103f6d7a52558daa143a284
lcgamboa <lcgamboa@users.noreply.github.com>
2021-10-22 14:03:18 -0300

    Add processor support for the C extension (#134)

--- src/processors/RISC-V/riscv.h
+++ src/processors/RISC-V/riscv.h
@@ -45,6 +45,7 @@ Enum(CompOp, NOP, EQ, NE, LT, LTU, GE, GEU);
 Enum(MemOp, NOP, LB, LH, LW, LBU, LHU, SB, SH, SW, LWU, LD, SD);
 Enum(ECALL, none, print_int = 1, print_char = 2, print_string = 4, exit = 10);
 Enum(PcSrc, PC4 = 0, ALU = 1);
+Enum(PcInc, INC2 = 0, INC4 = 1);
 
 /** Instruction field parser */
 class RVInstrParser {
@@ -61,6 +62,16 @@ public:
     std::vector<uint32_t> decodeR32Instr(const uint32_t& instr) const { return m_decodeR32Instr(instr); }
     std::vector<uint32_t> decodeB32Instr(const uint32_t& instr) const { return m_decodeB32Instr(instr); }
 
+    // RVC
+    std::vector<uint32_t> decodeCA16Instr(const uint32_t& instr) const { return m_decodeCA16Instr(instr); }
+    std::vector<uint32_t> decodeCI16Instr(const uint32_t& instr) const { return m_decodeCI16Instr(instr); }
+    std::vector<uint32_t> decodeCS16Instr(const uint32_t& instr) const { return m_decodeCS16Instr(instr); }
+    std::vector<uint32_t> decodeCIW16Instr(const uint32_t& instr) const { return m_decodeCIW16Instr(instr); }
+    std::vector<uint32_t> decodeCSS16Instr(const uint32_t& instr) const { return m_decodeCSS16Instr(instr); }
+    std::vector<uint32_t> decodeCJ16Instr(const uint32_t& instr) const { return m_decodeCJ16Instr(instr); }
+    std::vector<uint32_t> decodeCB16Instr(const uint32_t& instr) const { return m_decodeCB16Instr(instr); }
+    std::vector<uint32_t> decodeCB216Instr(const uint32_t& instr) const { return m_decodeCB216Instr(instr); }
+
 private:
     RVInstrParser() {
         m_decodeR32Instr = generateInstrParser<uint32_t>(std::vector<int>{7, 5, 3, 5, 5, 7});  // from LSB to MSB
@@ -69,6 +80,16 @@ private:
         m_decodeB32Instr = generateInstrParser<uint32_t>(std::vector<int>{7, 1, 4, 3, 5, 5, 6, 1});
         m_decodeU32Instr = generateInstrParser<uint32_t>(std::vector<int>{7, 5, 20});
         m_decodeJ32Instr = generateInstrParser<uint32_t>(std::vector<int>{7, 5, 8, 1, 10, 1});
+
+        // RVC
+        m_decodeCA16Instr = generateInstrParser<uint32_t>(std::vector<int>{2, 3, 2, 3, 2, 1, 3, 16});
+        m_decodeCI16Instr = generateInstrParser<uint32_t>(std::vector<int>{2, 5, 5, 1, 3, 16});
+        m_decodeCS16Instr = generateInstrParser<uint32_t>(std::vector<int>{2, 3, 2, 3, 3, 3, 16});
+        m_decodeCIW16Instr = generateInstrParser<uint32_t>(std::vector<int>{2, 3, 8, 3, 16});
+        m_decodeCSS16Instr = generateInstrParser<uint32_t>(std::vector<int>{2, 5, 6, 3, 16});
+        m_decodeCJ16Instr = generateInstrParser<uint32_t>(std::vector<int>{2, 11, 3, 16});
+        m_decodeCB16Instr = generateInstrParser<uint32_t>(std::vector<int>{2, 5, 3, 3, 3, 16});
+        m_decodeCB216Instr = generateInstrParser<uint32_t>(std::vector<int>{2, 5, 3, 2, 1, 3, 16});
     }
     decode_functor<uint32_t> m_decodeU32Instr;
     decode_functor<uint32_t> m_decodeJ32Instr;
@@ -76,6 +97,16 @@ private:
     decode_functor<uint32_t> m_decodeS32Instr;
     decode_functor<uint32_t> m_decodeR32Instr;
     decode_functor<uint32_t> m_decodeB32Instr;
+
+    // RVC
+    decode_functor<uint32_t> m_decodeCA16Instr;
+    decode_functor<uint32_t> m_decodeCI16Instr;
+    decode_functor<uint32_t> m_decodeCS16Instr;
+    decode_functor<uint32_t> m_decodeCIW16Instr;
+    decode_functor<uint32_t> m_decodeCSS16Instr;
+    decode_functor<uint32_t> m_decodeCJ16Instr;
+    decode_functor<uint32_t> m_decodeCB16Instr;
+    decode_functor<uint32_t> m_decodeCB216Instr;
 };
 
 }  // namespace Ripes
