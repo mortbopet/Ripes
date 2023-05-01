6983ca2afbc2d3f8899c256ffd35113d02ca94f4
lcgamboa <lcgamboa@yahoo.com>
2023-05-01 10:11:01 -0300

    fix QMutex crash

--- src/io/ioexternalbus.cpp
+++ src/io/ioexternalbus.cpp
@@ -21,228 +21,236 @@
 
 namespace Ripes {
 
- static uint64_t simtime=0;   //FIXME use intruction counter
+static uint64_t simtime = 0; // FIXME use instruction counter
 
-IOExternalBus::IOExternalBus(QWidget* parent)
-    : IOBase(IOType::EXTERNALBUS, parent), m_ui(new Ui::IOExternalBus), tcpSocket(new XTcpSocket()) {
-    m_ByteSize = 0x46;
-    skt_use.unlock();
-
-    m_ui->setupUi(this);
-    connect(m_ui->connectButton, &QPushButton::clicked, this, &IOExternalBus::connectButtonTriggered);
+IOExternalBus::IOExternalBus(QWidget *parent)
+    : IOBase(IOType::EXTERNALBUS, parent), m_ui(new Ui::IOExternalBus),
+      tcpSocket(new XTcpSocket()) {
+  m_ByteSize = 0x46;
+  m_ui->setupUi(this);
+  connect(m_ui->connectButton, &QPushButton::clicked, this,
+          &IOExternalBus::connectButtonTriggered);
 }
 
 IOExternalBus::~IOExternalBus() {
-    unregister();
-    delete m_ui;
+  unregister();
+  delete m_ui;
 };
 
-unsigned IOExternalBus::byteSize() const {
-    return m_ByteSize;
-}
+unsigned IOExternalBus::byteSize() const { return m_ByteSize; }
 
 QString IOExternalBus::description() const {
-    return "An external bus is a memory mapped bus handled through network transactions. The peripheral connects to an "
-           "IP address denoting a peripheral server - for more details, refer to the Ripes wiki.";
+  return "An external bus is a memory mapped bus handled through network "
+         "transactions. The peripheral connects to an "
+         "IP address denoting a peripheral server - for more details, refer to "
+         "the Ripes wiki.";
 }
 
 VInt IOExternalBus::ioRead(AInt offset, unsigned size) {
-    uint32_t rvalue = 0;
+  uint32_t rvalue = 0;
 
-    if (tcpSocket->isOpen()) {
-        simtime+=100000000L;
-        skt_use.lock();
-        uint32_t payload = htonl(offset);
-        QByteArray dp = QByteArray(reinterpret_cast<const char*>(&payload), 4);
-        if (send_cmd(VBUS::VB_PREAD, 4, dp, simtime) < 0) {
-            skt_use.unlock();
-            return 0;
-        }
-
-        VBUS::CmdHeader cmd_header;
-
-        if (recv_cmd(cmd_header) < 0) {
-            skt_use.unlock();
-            return 0;
-        }
-
-        if (cmd_header.payload_size) {
-            dp = QByteArray(8, 0);
-            if (recv_payload(dp, 8) < 0) {
-                skt_use.unlock();
-                return 0;
-            }
-            uint32_t* payload = reinterpret_cast<uint32_t*>(dp.data());
-
-            for (uint32_t i = 0; i < 2; i++) {
-                payload[i] = ntohl(payload[i]);
-            }
-            rvalue = payload[1];
-        } else {
-            disconnectOnError("read error");
-        }
-        skt_use.unlock();
+  if (tcpSocket->isOpen()) {
+    simtime += 100000000L;
+    skt_use.lock();
+    uint32_t payload = htonl(offset);
+    QByteArray dp = QByteArray(reinterpret_cast<const char *>(&payload), 4);
+    if (send_cmd(VBUS::VB_PREAD, 4, dp, simtime) < 0) {
+      return 0;
     }
-    return rvalue;
+
+    VBUS::CmdHeader cmd_header;
+
+    if (recv_cmd(cmd_header) < 0) {
+      return 0;
+    }
+
+    if (cmd_header.payload_size) {
+      dp = QByteArray(8, 0);
+      if (recv_payload(dp, 8) < 0) {
+        skt_use.unlock();
+        return 0;
+      }
+      uint32_t *payload = reinterpret_cast<uint32_t *>(dp.data());
+
+      for (uint32_t i = 0; i < 2; i++) {
+        payload[i] = ntohl(payload[i]);
+      }
+      rvalue = payload[1];
+    } else {
+      disconnectOnError("read error");
+    }
+    skt_use.unlock();
+  }
+  return rvalue;
 }
 
 void IOExternalBus::ioWrite(AInt offset, VInt value, unsigned size) {
-    if (tcpSocket->isOpen()) {
-        simtime+=100000000L;
-        skt_use.lock();
-        uint32_t payload[2];
-        payload[0] = htonl(offset);
-        payload[1] = htonl(value);
-        QByteArray dp = QByteArray(reinterpret_cast<char*>(payload), 8);
-        if (send_cmd(VBUS::VB_PWRITE, 8, dp, simtime) < 0) {
-            skt_use.unlock();
-            return;
-        }
-
-        VBUS::CmdHeader cmd_header;
-
-        if (recv_cmd(cmd_header) < 0) {
-            skt_use.unlock();
-            return;
-        }
-
-        if (cmd_header.msg_type != VBUS::VB_PWRITE) {
-            disconnectOnError("write error");
-        }
-        skt_use.unlock();
+  if (tcpSocket->isOpen()) {
+    simtime += 100000000L;
+    skt_use.lock();
+    uint32_t payload[2];
+    payload[0] = htonl(offset);
+    payload[1] = htonl(value);
+    QByteArray dp = QByteArray(reinterpret_cast<char *>(payload), 8);
+    if (send_cmd(VBUS::VB_PWRITE, 8, dp, simtime) < 0) {
+      return;
     }
+
+    VBUS::CmdHeader cmd_header;
+
+    if (recv_cmd(cmd_header) < 0) {
+      return;
+    }
+
+    if (cmd_header.msg_type != VBUS::VB_PWRITE) {
+      disconnectOnError("write error");
+    }
+    skt_use.unlock();
+  }
 }
 
 void IOExternalBus::connectButtonTriggered() {
-    if (!m_Connected) {
-        tcpSocket->abort();
-        if (tcpSocket->connectToHost(m_ui->address->text(), m_ui->port->value())) {
-            if (send_cmd(VBUS::VB_PINFO, 0, {}, simtime) < 0) {
-                disconnectOnError();
-                return;
-            }
-            VBUS::CmdHeader cmd_header;
+  if (!m_Connected) {
+    tcpSocket->abort();
+    skt_use.lock();
+    if (tcpSocket->connectToHost(m_ui->address->text(), m_ui->port->value())) {
+      if (send_cmd(VBUS::VB_PINFO, 0, {}, simtime) < 0) {
+        return;
+      }
+      VBUS::CmdHeader cmd_header;
 
-            if (recv_cmd(cmd_header) < 0) {
-                disconnectOnError();
-                return;
-            }
-            if (cmd_header.payload_size) {
-                QByteArray buff(cmd_header.payload_size + 1, 0);
-                if (recv_payload(buff, cmd_header.payload_size) < 0) {
-                    disconnectOnError();
-                    return;
-                }
-                QJsonParseError error;
-                QJsonDocument desc = QJsonDocument::fromJson(buff.data(), &error);
-                if (error.error == QJsonParseError::NoError) {
-                    const unsigned int addrw = desc.object().value(QString("address width")).toInt();
-                    QJsonObject osymbols = desc.object().value(QString("symbols")).toObject();
-
-                    m_regDescs.clear();
-                    for (QJsonObject::iterator i = osymbols.begin(); i != osymbols.end(); i++) {
-                        m_regDescs.push_back(
-                            RegDesc{i.key(), RegDesc::RW::RW, addrw * 8, static_cast<AInt>(i.value().toInt()), true});
-                    }
-                    m_ByteSize = addrw * osymbols.count();
-                    updateConnectionStatus(true, desc.object().value(QString("name")).toString());
-
-                } else {
-                    QMessageBox::information(nullptr, tr("Ripes VBus"), QString("json: ") + error.errorString());
-                    updateConnectionStatus(false);
-                    send_cmd(VBUS::VB_QUIT, 0, {}, simtime);
-                    tcpSocket->abort();
-                }
-            }
-        } else {
-            disconnectOnError();
+      if (recv_cmd(cmd_header) < 0) {
+        return;
+      }
+      if (cmd_header.payload_size) {
+        QByteArray buff(cmd_header.payload_size + 1, 0);
+        if (recv_payload(buff, cmd_header.payload_size) < 0) {
+          disconnectOnError();
+          return;
         }
-    } else {  // disconnect
-        updateConnectionStatus(false);
-        send_cmd(VBUS::VB_QUIT, 0, {}, simtime);
-        tcpSocket->abort();
-        m_regDescs.clear();
+        QJsonParseError error;
+        QJsonDocument desc = QJsonDocument::fromJson(buff.data(), &error);
+        if (error.error == QJsonParseError::NoError) {
+          const unsigned int addrw =
+              desc.object().value(QString("address width")).toInt();
+          QJsonObject osymbols =
+              desc.object().value(QString("symbols")).toObject();
+
+          m_regDescs.clear();
+          for (QJsonObject::iterator i = osymbols.begin(); i != osymbols.end();
+               i++) {
+            m_regDescs.push_back(RegDesc{i.key(), RegDesc::RW::RW, addrw * 8,
+                                         static_cast<AInt>(i.value().toInt()),
+                                         true});
+          }
+          m_ByteSize = addrw * osymbols.count();
+          updateConnectionStatus(
+              true, desc.object().value(QString("name")).toString());
+
+        } else {
+          QMessageBox::information(nullptr, tr("Ripes VBus"),
+                                   QString("json: ") + error.errorString());
+          updateConnectionStatus(false);
+          send_cmd(VBUS::VB_QUIT, 0, {}, simtime);
+          tcpSocket->abort();
+        }
+        skt_use.unlock();
+      }
+    } else {
+      disconnectOnError();
     }
-    emit regMapChanged();
-    emit sizeChanged();
+  } else { // disconnect
+    updateConnectionStatus(false);
+    send_cmd(VBUS::VB_QUIT, 0, {}, simtime);
+    tcpSocket->abort();
+    m_regDescs.clear();
+  }
+  emit regMapChanged();
+  emit sizeChanged();
 }
 
 void IOExternalBus::updateConnectionStatus(bool connected, QString Server) {
-    m_Connected = connected;
-    if (m_Connected) {
-        m_ui->connectButton->setText("Disconnect");
-        m_ui->status->setText("Connected");
-        m_ui->server->setText(Server);
-    } else {
-        m_ui->connectButton->setText("Connect");
-        m_ui->status->setText("Disconnected");
-        m_ui->server->setText(Server);
-    }
+  m_Connected = connected;
+  if (m_Connected) {
+    m_ui->connectButton->setText("Disconnect");
+    m_ui->status->setText("Connected");
+    m_ui->server->setText(Server);
+  } else {
+    m_ui->connectButton->setText("Connect");
+    m_ui->status->setText("Disconnected");
+    m_ui->server->setText(Server);
+  }
 }
 
-int32_t IOExternalBus::send_cmd(const uint32_t cmd, const uint32_t payload_size, const QByteArray& payload,const uint64_t time ) {
-    int32_t ret = -1;
-    VBUS::CmdHeader cmd_header;
+int32_t IOExternalBus::send_cmd(const uint32_t cmd, const uint32_t payload_size,
+                                const QByteArray &payload,
+                                const uint64_t time) {
+  int32_t ret = -1;
+  VBUS::CmdHeader cmd_header;
 
-    cmd_header.msg_type = htonl(cmd);
-    cmd_header.payload_size = htonl(payload_size);
-    cmd_header.time = htonll(time);
+  cmd_header.msg_type = htonl(cmd);
+  cmd_header.payload_size = htonl(payload_size);
+  cmd_header.time = htonll(time);
 
-    QByteArray dp = QByteArray(reinterpret_cast<const char*>(&cmd_header), sizeof(VBUS::CmdHeader));
+  QByteArray dp = QByteArray(reinterpret_cast<const char *>(&cmd_header),
+                             sizeof(VBUS::CmdHeader));
 
-    if (payload_size) {
-        dp.append(payload);
-    }
+  if (payload_size) {
+    dp.append(payload);
+  }
 
-    if ((ret = tcpSocket->write(dp, dp.size())) < 0) {
-        disconnectOnError();
-        return -1;
-    }
+  if ((ret = tcpSocket->write(dp, dp.size())) < 0) {
+    disconnectOnError();
+    return -1;
+  }
 
-    return ret;
+  return ret;
 }
 
-int32_t IOExternalBus::recv_cmd(VBUS::CmdHeader& cmd_header) {
-    QByteArray dp = QByteArray(reinterpret_cast<char*>(&cmd_header), sizeof(VBUS::CmdHeader));
-    int ret = tcpSocket->read(dp, sizeof(VBUS::CmdHeader));
-    if (ret < 0) {
-        disconnectOnError();
-        return -1;
-    }
+int32_t IOExternalBus::recv_cmd(VBUS::CmdHeader &cmd_header) {
+  QByteArray dp = QByteArray(reinterpret_cast<char *>(&cmd_header),
+                             sizeof(VBUS::CmdHeader));
+  int ret = tcpSocket->read(dp, sizeof(VBUS::CmdHeader));
+  if (ret < 0) {
+    disconnectOnError();
+    return -1;
+  }
 
-    VBUS::CmdHeader* hr = reinterpret_cast<VBUS::CmdHeader*>(dp.data());
+  VBUS::CmdHeader *hr = reinterpret_cast<VBUS::CmdHeader *>(dp.data());
 
-    cmd_header.msg_type = ntohl(hr->msg_type);
-    cmd_header.payload_size = ntohl(hr->payload_size);
-    cmd_header.time = ntohll(cmd_header.time);
+  cmd_header.msg_type = ntohl(hr->msg_type);
+  cmd_header.payload_size = ntohl(hr->payload_size);
+  cmd_header.time = ntohll(cmd_header.time);
 
-    return ret;
+  return ret;
 }
 
-int32_t IOExternalBus::recv_payload(QByteArray& buff, const uint32_t payload_size) {
-    int ret = tcpSocket->read(buff, payload_size);
-    if (ret < 0) {
-        disconnectOnError();
-        return -1;
-    }
-    return ret;
+int32_t IOExternalBus::recv_payload(QByteArray &buff,
+                                    const uint32_t payload_size) {
+  int ret = tcpSocket->read(buff, payload_size);
+  if (ret < 0) {
+    disconnectOnError();
+    return -1;
+  }
+  return ret;
 }
 
 void IOExternalBus::disconnectOnError(QString msg) {
-    skt_use.unlock();
+  skt_use.unlock();
 
-    if (msg.length() > 1) {
-        QMessageBox::information(nullptr, tr("Ripes VBus"), msg);
-    } else {
-        QMessageBox::information(nullptr, tr("Ripes VBus"), tcpSocket->getLastErrorStr());
-    }
-    tcpSocket->close();
-    updateConnectionStatus(false);
-    tcpSocket->abort();
-    m_regDescs.clear();
+  if (msg.length() > 1) {
+    QMessageBox::information(nullptr, tr("Ripes VBus"), msg);
+  } else {
+    QMessageBox::information(nullptr, tr("Ripes VBus"),
+                             tcpSocket->getLastErrorStr());
+  }
+  tcpSocket->close();
+  updateConnectionStatus(false);
+  tcpSocket->abort();
+  m_regDescs.clear();
 
-    emit regMapChanged();
-    emit sizeChanged();
+  emit regMapChanged();
+  emit sizeChanged();
 }
 
-}  // namespace Ripes
+} // namespace Ripes
--- test/tst_xtcpsocket.cpp
+++ test/tst_xtcpsocket.cpp
@@ -1,56 +1,57 @@
 #include <QFuture>
+#include <QFutureWatcher>
 #include <QtConcurrent/QtConcurrent>
 #include <QtTest/QTest>
 
 #include "io/XTcpSocket.h"
 
 class tst_xtcpsocket : public QObject {
-    Q_OBJECT
+  Q_OBJECT
 
 private:
-    XTcpSocket client;
-    XTcpSocket server;
+  XTcpSocket client;
+  XTcpSocket server;
 
 private slots:
-    void tst_pinpong();
+  void tst_pinpong();
 };
 
 void tst_xtcpsocket::tst_pinpong() {
-    const QByteArray in("0123456789", 10);
-    QByteArray out(10, 0);
+  const QByteArray in("0123456789", 10);
+  QByteArray out(10, 0);
 
-    if (server.serverStart(7890) < 0) {
-        QFAIL(server.getLastErrorStr().toStdString().c_str());
-    }
-    QFuture<void> future = QtConcurrent::run(&server, &XTcpSocket::serverAccept);
-    if (client.connectToHost("127.0.0.1", 7890) < 0) {
-        QFAIL(client.getLastErrorStr().toStdString().c_str());
-    }
-    future.waitForFinished();
+  if (server.serverStart(7890) < 0) {
+    QFAIL(server.getLastErrorStr().toStdString().c_str());
+  }
+  QFuture<int> future = QtConcurrent::run(&XTcpSocket::serverAccept, &server);
+  if (client.connectToHost("127.0.0.1", 7890) < 0) {
+    QFAIL(client.getLastErrorStr().toStdString().c_str());
+  }
+  future.waitForFinished();
 
-    out.fill(' ', 10);
-    if (server.write(in, 10) < 0) {
-        QFAIL(server.getLastErrorStr().toStdString().c_str());
-    }
-    if (client.read(out, 10) < 0) {
-        QFAIL(client.getLastErrorStr().toStdString().c_str());
-    }
+  out.fill(' ', 10);
+  if (server.write(in, 10) < 0) {
+    QFAIL(server.getLastErrorStr().toStdString().c_str());
+  }
+  if (client.read(out, 10) < 0) {
+    QFAIL(client.getLastErrorStr().toStdString().c_str());
+  }
 
-    QCOMPARE(in, out);
+  QCOMPARE(in, out);
 
-    out.fill(' ', 10);
-    if (client.write(in, 10) < 0) {
-        QFAIL(client.getLastErrorStr().toStdString().c_str());
-    }
-    if (server.read(out, 10) < 0) {
-        QFAIL(server.getLastErrorStr().toStdString().c_str());
-    }
+  out.fill(' ', 10);
+  if (client.write(in, 10) < 0) {
+    QFAIL(client.getLastErrorStr().toStdString().c_str());
+  }
+  if (server.read(out, 10) < 0) {
+    QFAIL(server.getLastErrorStr().toStdString().c_str());
+  }
 
-    QCOMPARE(in, out);
+  QCOMPARE(in, out);
 
-    server.serverClose();
-    server.close();
-    client.close();
+  server.serverClose();
+  server.close();
+  client.close();
 }
 
 QTEST_APPLESS_MAIN(tst_xtcpsocket)
