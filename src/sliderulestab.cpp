b195e71879b19303d95093c3c5168c31f04853ba
Ryan Cohen <rcohenprogramming@gmail.com>
2024-02-26 09:52:46 -0500

    Remove complex code to start with a simple example

--- src/sliderulestab.cpp
+++ src/sliderulestab.cpp
@@ -5,73 +5,20 @@
 
 namespace Ripes {
 
-ISAInstructionsModel::ISAInstructionsModel(QObject *parent) {
-  changeISAInfo(*ProcessorHandler::currentISA());
-}
+ISAInstructionsModel::ISAInstructionsModel(QObject *parent) {}
 
-int ISAInstructionsModel::rowCount(const QModelIndex &) const {
-  if (!m_isaInfo) {
-    return 0;
-  }
+int ISAInstructionsModel::rowCount(const QModelIndex &) const { return 0; }
 
-  return m_isaInfo->instructions().size();
-}
-
-int ISAInstructionsModel::columnCount(const QModelIndex &) const { return 2; }
+int ISAInstructionsModel::columnCount(const QModelIndex &) const { return 0; }
 
 QVariant ISAInstructionsModel::data(const QModelIndex &index, int role) const {
-  if (!m_isaInfo || index.row() >= rowCount(QModelIndex())) {
-    return QVariant();
-  }
-
-  if (role == Qt::DisplayRole) {
-    return m_isaInfo->instructions().at(index.row())->name();
-  }
-
-  return QVariant();
+  return {};
 }
 
 QVariant ISAInstructionsModel::headerData(int section,
                                           Qt::Orientation orientation,
                                           int role) const {
   return {};
-}
-
-const ISAInfoBase *ISAInstructionsModel::isaInfo() const {
-  return m_isaInfo.get();
-}
-
-const ISAInfoBase *ISAInstructionsModel::prevISAInfo() const {
-  return m_prevIsaInfo.get();
-}
-
-void ISAInstructionsModel::changeISAFamily(ISAFamily isaFamily) {
-  if (!m_isaInfo || m_isaInfo->isaFamily() != isaFamily) {
-    changeISA(*ISAFamilySets.at(isaFamily).begin());
-  }
-}
-
-void ISAInstructionsModel::changeISA(ISA isa) {
-  if (!m_isaInfo || m_isaInfo->isaID() != isa) {
-    beginResetModel();
-    m_prevIsaInfo = m_isaInfo;
-    m_isaInfo = ISAInfoRegistry::getISA(isa, QStringList());
-    modelChanged();
-  }
-}
-
-void ISAInstructionsModel::changeISAInfo(const ISAInfoBase &isaInfo) {
-  if (!m_isaInfo || m_isaInfo->eq(&isaInfo, isaInfo.enabledExtensions())) {
-    beginResetModel();
-    m_prevIsaInfo = m_isaInfo;
-    m_isaInfo = ISAInfoRegistry::getISA(isaInfo.isaID(), QStringList());
-    modelChanged();
-  }
-}
-
-void ISAInstructionsModel::modelChanged() {
-  endResetModel();
-  emit isaInfoChanged(*m_isaInfo);
 }
 
 SliderulesTab::SliderulesTab(QToolBar *toolbar, QWidget *parent)
@@ -88,63 +35,29 @@ SliderulesTab::SliderulesTab(QToolBar *toolbar, QWidget *parent)
                           QVariant(static_cast<int>(familyName.first)));
   }
 
-  m_encodingModel = std::make_unique<ISAInstructionsModel>();
-  ui->encodingTable->setModel(m_encodingModel.get());
+  const auto &isaInfo = *ISAInfoRegistry::getISA(ISA::RV32I, QStringList());
+  isaFamilyBox->setCurrentIndex(
+      isaFamilyBox->findData(QVariant(static_cast<int>(isaInfo.isaFamily()))));
 
-  connect(isaFamilyBox, &QComboBox::activated, this, [=](int index) {
-    m_encodingModel->changeISAFamily(
-        static_cast<ISAFamily>(isaFamilyBox->itemData(index).toInt()));
-  });
-  connect(isaBox, &QComboBox::activated, this, [=](int index) {
-    m_encodingModel->changeISA(
-        static_cast<ISA>(isaBox->itemData(index).toInt()));
-  });
-  connect(mainExtBox, &QComboBox::activated, this, [=](int index) {
-    auto ext = mainExtBox->itemData(index).toString();
-    const auto *isaInfo = m_encodingModel->isaInfo();
-    auto enabledExts = isaInfo->enabledExtensions();
-    if (ext != isaInfo->baseExtension() && !enabledExts.contains(ext)) {
-      enabledExts << ext;
-      m_encodingModel->changeISAInfo(
-          *ISAInfoRegistry::getISA(isaInfo->isaID(), enabledExts));
-    }
-  });
+  isaBox->clear();
+  for (const auto &isa : ISAFamilySets.at(isaInfo.isaFamily())) {
+    isaBox->addItem(ISANames.at(isa), QVariant(static_cast<int>(isa)));
+  }
 
-  connect(m_encodingModel.get(), &ISAInstructionsModel::isaInfoChanged, this,
-          &SliderulesTab::updateView);
+  isaBox->setCurrentIndex(
+      isaBox->findData(QVariant(static_cast<int>(isaInfo.isaID()))));
 
-  updateView(*m_encodingModel->isaInfo());
+  mainExtBox->clear();
+  for (const auto &ext :
+       QStringList(isaInfo.baseExtension()) + isaInfo.supportedExtensions()) {
+    mainExtBox->addItem(ext, ext);
+  }
+
+  baseExtCheckBox->setText(isaInfo.baseExtension());
+  baseExtCheckBox->setChecked(true);
+  baseExtCheckBox->setEnabled(false);
 }
 
 SliderulesTab::~SliderulesTab() { delete ui; }
-
-void SliderulesTab::updateView(const ISAInfoBase &isaInfo) {
-  const auto *prevISAInfo = m_encodingModel->prevISAInfo();
-
-  if (!prevISAInfo || isaInfo.isaFamily() != prevISAInfo->isaFamily()) {
-    isaFamilyBox->setCurrentIndex(isaFamilyBox->findData(
-        QVariant(static_cast<int>(isaInfo.isaFamily()))));
-
-    isaBox->clear();
-    for (const auto &isa : ISAFamilySets.at(isaInfo.isaFamily())) {
-      isaBox->addItem(ISANames.at(isa), QVariant(static_cast<int>(isa)));
-    }
-  }
-
-  if (!prevISAInfo || isaInfo.isaID() != prevISAInfo->isaID()) {
-    isaBox->setCurrentIndex(
-        isaBox->findData(QVariant(static_cast<int>(isaInfo.isaID()))));
-
-    mainExtBox->clear();
-    for (const auto &ext :
-         QStringList(isaInfo.baseExtension()) + isaInfo.supportedExtensions()) {
-      mainExtBox->addItem(ext, ext);
-    }
-
-    baseExtCheckBox->setText(isaInfo.baseExtension());
-    baseExtCheckBox->setChecked(true);
-    baseExtCheckBox->setEnabled(false);
-  }
-}
 
 } // namespace Ripes
--- src/sliderulestab.h
+++ src/sliderulestab.h
@@ -25,23 +25,6 @@ public:
   ISAInstructionsModel(QObject *parent = nullptr);
   ~ISAInstructionsModel() = default;
 
-  // TODO(raccog): This could be a variable that is controlable in the UI.
-  constexpr static size_t BIT_COLUMNS = 32;
-
-  enum Column {
-    EXTENSION = 0,
-    TYPE,
-    DESCRIPTION,
-    OPCODE,
-    // TODO(raccog): The field columns should be variable based on the maximum
-    // number of fields
-    FIELD0,
-    FIELD1,
-    FIELD2,
-    BIT_START,
-    BIT_END = BIT_START + BIT_COLUMNS
-  };
-
   virtual int rowCount(const QModelIndex &) const override;
   virtual int columnCount(const QModelIndex &) const override;
   virtual QVariant data(const QModelIndex &index,
@@ -49,22 +32,20 @@ public:
   virtual QVariant headerData(int section, Qt::Orientation orientation,
                               int role = Qt::DisplayRole) const override;
 
-  const ISAInfoBase *isaInfo() const;
-  const ISAInfoBase *prevISAInfo() const;
+  //   const ISAInfoBase *isaInfo() const;
+  //   const ISAInfoBase *prevISAInfo() const;
 
-signals:
-  void isaInfoChanged(const ISAInfoBase &isaInfo);
+  // signals:
+  //   void isaInfoChanged(const ISAInfoBase &isaInfo);
 
-public slots:
-  void changeISAFamily(ISAFamily isaFamily);
-  void changeISA(ISA isa);
-  void changeISAInfo(const ISAInfoBase &isaInfo);
+  // public slots:
+  //   void changeISAFamily(ISAFamily isaFamily);
+  //   void changeISA(ISA isa);
+  //   void changeISAInfo(const ISAInfoBase &isaInfo);
 
 protected:
-  void modelChanged();
-
-  std::shared_ptr<const ISAInfoBase> m_isaInfo = nullptr;
-  std::shared_ptr<const ISAInfoBase> m_prevIsaInfo = nullptr;
+  // std::shared_ptr<const ISAInfoBase> m_isaInfo = nullptr;
+  // std::shared_ptr<const ISAInfoBase> m_prevIsaInfo = nullptr;
 };
 
 class SliderulesTab : public RipesTab {
@@ -74,7 +55,7 @@ public:
   ~SliderulesTab();
 
 public slots:
-  void updateView(const ISAInfoBase &isaInfo);
+  // void updateView(const ISAInfoBase &isaInfo);
 
 private:
   Ui::SliderulesTab *ui = nullptr;
@@ -84,7 +65,7 @@ private:
   QComboBox *mainExtBox = nullptr;
   QCheckBox *baseExtCheckBox = nullptr;
 
-  std::unique_ptr<ISAInstructionsModel> m_encodingModel = nullptr;
+  std::unique_ptr<ISAInstructionsModel> m_isaModel = nullptr;
 };
 
 } // namespace Ripes
