c716784a417f09845e844b9efaedc1e8043ef393
Ryan Cohen <rcohenprogramming@gmail.com>
2024-02-27 12:37:06 -0500

    Get a working model for a QTreeView

--- src/sliderulestab.cpp
+++ src/sliderulestab.cpp
@@ -5,20 +5,88 @@
 
 namespace Ripes {
 
-ISAInstructionsModel::ISAInstructionsModel(QObject *parent) {}
+InstructionItem::InstructionItem(const InstructionBase &instruction,
+                                 InstructionItem *parent)
+    : m_instruction(instruction), m_parent(parent) {
+  if (!parent)
+    m_children.push_back(std::make_unique<InstructionItem>(instruction, this));
+}
 
-int ISAInstructionsModel::rowCount(const QModelIndex &) const { return 0; }
+InstructionItem *InstructionItem::child() {
+  return m_children.size() > 0 ? m_children.at(0).get() : nullptr;
+}
 
-int ISAInstructionsModel::columnCount(const QModelIndex &) const { return 0; }
+const InstructionBase &InstructionItem::data() const { return m_instruction; }
+
+InstructionItem *InstructionItem::parent() { return m_parent; }
+
+bool InstructionItem::hasParent() const { return m_parent; }
+
+ISAInstructionsModel::ISAInstructionsModel(QObject *parent) {
+  for (const auto &instr : ProcessorHandler::currentISA()->instructions()) {
+    m_instrItems.push_back(std::make_unique<InstructionItem>(*instr.get()));
+  }
+}
+
+int ISAInstructionsModel::rowCount(const QModelIndex &parent) const {
+  if (parent.isValid()) {
+    return parent.parent().isValid() ? 0 : 1;
+  }
+  return m_instrItems.size();
+}
+
+int ISAInstructionsModel::columnCount(const QModelIndex &) const { return 1; }
 
 QVariant ISAInstructionsModel::data(const QModelIndex &index, int role) const {
-  return {};
+  if (!index.isValid() || role != Qt::DisplayRole) {
+    return {};
+  }
+
+  const auto *item =
+      static_cast<const InstructionItem *>(index.internalPointer());
+  return !item->hasParent() ? item->data().name() : item->data().description();
+}
+
+Qt::ItemFlags ISAInstructionsModel::flags(const QModelIndex &index) const {
+  return index.isValid() ? QAbstractItemModel::flags(index)
+                         : Qt::ItemFlags(Qt::NoItemFlags);
 }
 
 QVariant ISAInstructionsModel::headerData(int section,
                                           Qt::Orientation orientation,
                                           int role) const {
   return {};
+}
+
+QModelIndex ISAInstructionsModel::index(int row, int column,
+                                        const QModelIndex &parent) const {
+  if (!hasIndex(row, column, parent)) {
+    return {};
+  }
+
+  if (parent.isValid()) {
+    if (auto *child =
+            static_cast<InstructionItem *>(parent.internalPointer())->child()) {
+      return createIndex(row, column, child);
+    }
+  } else {
+    if (static_cast<size_t>(row) < m_instrItems.size()) {
+      return createIndex(row, column, m_instrItems.at(row).get());
+    }
+  }
+
+  return {};
+}
+
+QModelIndex ISAInstructionsModel::parent(const QModelIndex &index) const {
+  if (!index.isValid()) {
+    return {};
+  }
+
+  auto *item = static_cast<InstructionItem *>(index.internalPointer());
+  auto *parent = item->parent();
+
+  return parent ? createIndex(0, 0, parent) : QModelIndex{};
 }
 
 SliderulesTab::SliderulesTab(QToolBar *toolbar, QWidget *parent)
@@ -29,6 +97,9 @@ SliderulesTab::SliderulesTab(QToolBar *toolbar, QWidget *parent)
   isaBox = ui->isaBox;
   mainExtBox = ui->mainExtBox;
   baseExtCheckBox = ui->baseExtCheckBox;
+
+  m_isaModel = std::make_unique<ISAInstructionsModel>();
+  ui->encodingTable->setModel(m_isaModel.get());
 
   for (const auto &familyName : ISAFamilyNames) {
     isaFamilyBox->addItem(familyName.second,
--- src/sliderulestab.h
+++ src/sliderulestab.h
@@ -19,7 +19,24 @@ namespace Ui {
 class SliderulesTab;
 }
 
-class ISAInstructionsModel : public QStandardItemModel {
+class InstructionItem {
+public:
+  explicit InstructionItem(const InstructionBase &instruction,
+                           InstructionItem *parent = nullptr);
+
+  InstructionItem *child();
+  // int childCount() const;
+  const InstructionBase &data() const;
+  InstructionItem *parent();
+  bool hasParent() const;
+
+private:
+  std::vector<std::unique_ptr<InstructionItem>> m_children;
+  const InstructionBase &m_instruction;
+  InstructionItem *m_parent;
+};
+
+class ISAInstructionsModel : public QAbstractItemModel {
   Q_OBJECT
 public:
   ISAInstructionsModel(QObject *parent = nullptr);
@@ -29,8 +46,12 @@ public:
   virtual int columnCount(const QModelIndex &) const override;
   virtual QVariant data(const QModelIndex &index,
                         int role = Qt::DisplayRole) const override;
+  virtual Qt::ItemFlags flags(const QModelIndex &index) const override;
   virtual QVariant headerData(int section, Qt::Orientation orientation,
                               int role = Qt::DisplayRole) const override;
+  virtual QModelIndex index(int row, int column,
+                            const QModelIndex &parent = {}) const override;
+  virtual QModelIndex parent(const QModelIndex &index) const override;
 
   //   const ISAInfoBase *isaInfo() const;
   //   const ISAInfoBase *prevISAInfo() const;
@@ -46,6 +67,8 @@ public:
 protected:
   // std::shared_ptr<const ISAInfoBase> m_isaInfo = nullptr;
   // std::shared_ptr<const ISAInfoBase> m_prevIsaInfo = nullptr;
+
+  std::vector<std::unique_ptr<InstructionItem>> m_instrItems;
 };
 
 class SliderulesTab : public RipesTab {
