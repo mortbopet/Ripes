025a727bc01b3da60afab9e68c597888fc814797
Kovalev Pavel <kovalev5690@gmail.com>
2024-03-21 13:00:44 +0300

    Add basic templates

--- lti_server/server/application/app.py
+++ lti_server/server/application/app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates")
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def main_page():
+    return render_template("main_page.html")
+
+
+@app.route("/task/<task_id>")
+def task(task_id):
+    return render_template("task.html", task_id=task_id)
