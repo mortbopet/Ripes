4b43fc0490824274b7ee46fd136205cada8dbf76
Kovalev Pavel <kovalev5690@gmail.com>
2024-03-21 13:41:39 +0300

    Explicitly specify http methods

--- lti_server/server/application/app.py
+++ lti_server/server/application/app.py
@@ -3,16 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder="../templates")
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def main_page():
     return render_template("main_page.html")
 
 
-@app.route("/task/<task_id>")
+@app.route("/task/<task_id>", methods=["GET"])
 def task(task_id):
     return render_template("task.html", task_id=task_id)
 
 
-@app.route("/user/<user_id>")
+@app.route("/user/<user_id>", methods=["GET"])
 def user_summary(user_id):
     return render_template("summary.html", user_id=user_id)
