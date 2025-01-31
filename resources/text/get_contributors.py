1fcfce8cf9ead1b6f2c11726ad6e1749fa6b76ec
Silvia González Rodríguez <silvia.gonzalez17@alu.uclm.es>
2025-01-31 21:09:50 +0100

    Convert contributors list script to Python

---
+++ resources/text/get_contributors.py
@@ -0,0 +1,16 @@
+import subprocess
+
+git_output = subprocess.run(
+    ["git", "shortlog", "-sn", "--no-merges"], capture_output=True, text=True
+)
+lines = git_output.stdout.splitlines()
+names = [line.partition("\t")[-1] for line in lines]
+
+text = '<p align="center" style="margin: 0">'
+for name in names:
+    text = text + name + "</br>\n"
+text = text + "</p>"
+
+file = open("gen_contributors.html", "w")
+file.write(text)
+file.close()
