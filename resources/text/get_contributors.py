c1388dbe04f9f3db38645938ccbf8a9b92bbb374
Silvia González Rodríguez <silvia.gonzalez17@alu.uclm.es>
2025-01-31 23:32:12 +0100

    Other fixes
    Add special Python command for Windows, specify HEAD for git shortlog
    (why did it work?)

--- resources/text/get_contributors.py
+++ resources/text/get_contributors.py
@@ -1,14 +1,14 @@
 import subprocess
 
 git_output = subprocess.run(
-    ["git", "shortlog", "-sn", "--no-merges"], capture_output=True, text=True
+    ["git", "shortlog", "HEAD", "-sn", "--no-merges"], capture_output=True, text=True
 )
 lines = git_output.stdout.splitlines()
 names = [line.partition("\t")[-1] for line in lines]
 
 text = ""
 for name in names:
-    text = text + "<p align=\"center\" style=\"margin: 0\">" + name + "</p>\n"
+    text = text + '<p align="center" style="margin: 0">' + name + "</p>\n"
 
 file = open("gen_contributors.html", "w")
 file.write(text)
