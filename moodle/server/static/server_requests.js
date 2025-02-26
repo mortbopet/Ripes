55ce7426ecb7899247cc920d80b12eea2bdd867b
Kirill Karpunin <kirill.karpunin20014@gmail.com>
2025-02-26 13:17:45 +0300

    [ADD] Add buttons to make GET and POST requests to server

---
+++ moodle/server/static/server_requests.js
@@ -0,0 +1,23 @@
+function server_get_request() {
+    alert('GET-запрос отправлен')
+    fetch('/', {
+        method: 'GET'
+    }).then((res) => {
+        if (res.ok) {
+            alert('GET-запрос успешно обработан')
+        }
+        alert('При обработке GET-запроса произошла ошибка')
+    })
+}
+
+function server_post_request() {
+    alert('POST-запрос отправлен')
+    fetch('/', {
+        method: 'POST'
+    }).then((res) => {
+        if (res.ok) {
+            alert('POST-запрос успешно обработан')
+        }
+        alert('При обработке POST-запроса произошла ошибка')
+    })
+}
