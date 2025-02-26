372f1f35f5f792eab17dc90794ce1327ba9abad9
jqnfxa <ivborisov@stud.etu.ru>
2025-02-26 17:34:52 +0300

    [ADD] post/get handle

--- moodle/server/app.py
+++ moodle/server/app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(
     __name__,
     template_folder="templates",
+    static_folder="static"
 )
 
 
 @app.route('/', methods=['GET', 'POST'])
 def main_page() -> str:
-    return render_template("index.html")
+    if request.method == 'POST':
+        print('пришел POST')
+        return render_template("index.html")
+    else:
+        print('пришел GET')
+        return render_template("index.html")
 
 
 if __name__ == '__main__':
