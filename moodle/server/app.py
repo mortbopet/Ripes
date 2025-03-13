ce5907674521e086770ff1a2cde0cb516c44f5ee
Kirill Karpunin <kirill.karpunin20014@gmail.com>
2025-03-13 20:11:19 +0300

    [ADD] Add logic to process request from moodle via lti protocol

--- moodle/server/app.py
+++ moodle/server/app.py
@@ -7,6 +7,31 @@ app = Flask(
 )
 
 
+@app.route('/lti', methods=['POST'])
+def lti_request() -> str:
+    """
+    A method that processes a request from Moodle.
+    Retrieves user data from the request and prints it to the console.
+
+    :return: A template rendering the page with Ripes.
+    """
+    lti_data = request.form
+
+    user_id = lti_data.get('user_id', 'No user ID')
+    full_name = lti_data.get('lis_person_name_full', 'Unknown user')
+    email = lti_data.get('lis_person_contact_email_primary', 'No email')
+    course_title = lti_data.get('context_title', 'No course title')
+    roles = lti_data.get('roles', 'No user roles')
+
+    print(f"User ID: {user_id}")
+    print(f"Full Name: {full_name}")
+    print(f"Email: {email}")
+    print(f"Course Title: {course_title}")
+    print(f"Roles: {roles}")
+
+    return render_template("index.html")
+
+
 @app.route('/', methods=['GET', 'POST'])
 def main_page() -> str:
     if request.method == 'POST':
@@ -18,4 +43,4 @@ def main_page() -> str:
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
