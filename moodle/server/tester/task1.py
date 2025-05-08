982eab8eb7455dfb5aefc138730bee9eabfa118a
Arttish <super.art-tish@yandex.ru>
2025-05-09 01:26:03 +0300

    [FIX] Task1 check

--- moodle/server/tester/task1.py
+++ moodle/server/tester/task1.py
@@ -5,10 +5,11 @@ import random
 class Task1(Task):
     def __init__(self, code_file):
         super().__init__(code_file, regs=True)
+        self.tests = self.__generate_tests()
 
     def __generate_tests(self) -> list[dict]:
         tests = []
-        for i in range(100):
+        for i in range(5):
             a = random.randint(0, 10)
             b = random.randint(0, 10)
             test = {
@@ -23,3 +24,11 @@ class Task1(Task):
     def __check_test(self, test: dict) -> bool:
         res = self.tester.run(test["input"], test["reginit"])
         return int(res['report']['registers']['x3']) == test["expected_regs"]['x3']
+    
+    def run(self) -> float:
+        passed = 0
+        for test in self.tests:
+            if self.__check_test(test):
+                passed += 1
+
+        return passed / len(self.tests)
