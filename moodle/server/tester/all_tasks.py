383b99c6c4d8ead874eb97d7a7242fab6ac31576
Arttish <super.art-tish@yandex.ru>
2025-05-11 15:52:10 +0300

    [ADD] function to get tasks

---
+++ moodle/server/tester/all_tasks.py
@@ -0,0 +1,14 @@
+from task import Task
+from task1 import Task1
+
+
+all_tasks = {
+    0: Task,
+    1: Task1
+}
+
+
+def get_task_by_id(task_id):
+    if task_id not in all_tasks:
+        raise ValueError(f"Task with id {task_id} does not exist")
+    return all_tasks[task_id]
