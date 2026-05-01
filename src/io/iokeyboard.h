00a7dd9c14112dde007b0575eb3717b131d3c421
Petr Yakushev <144445892+PetrYakushev@users.noreply.github.com>
2026-05-01 22:40:30 +0300

    Add files via upload

--- src/io/iokeyboard.h
+++ src/io/iokeyboard.h
@@ -1,68 +1,59 @@
 #pragma once
-#include "iobase.h"
-#include <QFrame>
+
 #include <QLabel>
 #include <QMap>
 #include <QMutex>
-#include <QPushButton>
 #include <QQueue>
-#include <QVBoxLayout>
 #include <QWidget>
+
+#include "iobase.h"
+
+QT_FORWARD_DECLARE_CLASS(QPushButton);
 
 namespace Ripes {
 
-class IOKeyboard : public IOBase {
-  Q_OBJECT
+    class IOKeyboard : public IOBase {
+        Q_OBJECT
 
-  enum Parameters { BUFSIZE };
+            enum Parameters { BUFSIZE };
 
-public:
-  IOKeyboard(QWidget *parent);
-  ~IOKeyboard() { unregister(); }
+    public:
+        IOKeyboard(QWidget* parent);
+        ~IOKeyboard() { unregister(); };
 
-  unsigned byteSize() const override { return 8; }
-  QString description() const override;
-  QString baseName() const override { return "Keyboard"; }
+        virtual unsigned byteSize() const override { return 8; }
+        virtual QString description() const override;
+        virtual QString baseName() const override { return "Keyboard"; }
 
-  const std::vector<RegDesc> &registers() const override { return m_regDescs; }
-  const std::vector<IOSymbol> *extraSymbols() const override {
-    return &m_extraSymbols;
-  }
+        virtual const std::vector<RegDesc>& registers() const override {
+            return m_regDescs;
+        };
+        virtual const std::vector<IOSymbol>* extraSymbols() const override {
+            return &m_extraSymbols;
+        }
 
-  VInt ioRead(AInt offset, unsigned size) override;
-  void ioWrite(AInt offset, VInt value, unsigned size) override;
-  void reset() override;
+        virtual VInt ioRead(AInt offset, unsigned size) override;
+        virtual void ioWrite(AInt offset, VInt value, unsigned size) override;
+        virtual void reset() override;
 
-protected:
-  void parameterChanged(unsigned) override;
-  void keyPressEvent(QKeyEvent *event) override;
+    protected:
+        virtual void parameterChanged(unsigned) override;
+        void keyPressEvent(QKeyEvent* event) override;
 
-private:
-  void updateLayout();
-  void enqueueKey(uint8_t ascii);
-  void refreshStatusLabel();
+    private:
+        void updateLayout();
+        void enqueueKey(uint8_t ascii);
+        void refreshStatusLabel();
 
-  QHBoxLayout *addKeyRow(QVBoxLayout *parent);
-  QPushButton *createKey(const QString &label, uint8_t ascii, int w = 36,
-                         int h = 36);
-  void flashKey(uint8_t ascii);
-  void clearFlash();
+        QQueue<uint8_t> m_keyBuffer;
+        mutable QMutex m_bufMutex;
+        uint8_t m_lastKey = 0;
 
-  QQueue<uint8_t> m_keyBuffer;
-  mutable QMutex m_bufMutex;
-  uint8_t m_lastKey = 0;
+        QLabel* m_statusLabel = nullptr;
+        QMap<uint8_t, QPushButton*> m_keys;
 
-  QLabel *m_lblData = nullptr;
-  QLabel *m_lblChar = nullptr;
-  QLabel *m_lblFifoCount = nullptr;
-  QMap<uint8_t, QPushButton *> m_keys;
-  QPushButton *m_flashedBtn = nullptr;
+        std::vector<RegDesc> m_regDescs;
+        std::vector<IOSymbol> m_extraSymbols;
+    };
 
-  QGridLayout *m_mainLayout = nullptr;
-  QLabel *m_statusLabel = nullptr;
-
-  std::vector<RegDesc> m_regDescs;
-  std::vector<IOSymbol> m_extraSymbols;
-};
-
-} // namespace Ripes
+}
