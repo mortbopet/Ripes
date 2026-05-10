2dd49163fca5bd9a04f598eee8286a67ed38b764
Muhammed Enes Karaca <eneskaracap@gmail.com>
2026-05-10 16:35:25 +0300

    use -exec action instead of -executable test for find

--- .github/workflows/scripts/run_tests.sh
+++ .github/workflows/scripts/run_tests.sh
@@ -3,7 +3,7 @@
 # Fail on failing QTest execution
 set -e
 
-find . -executable -type f -name "tst_*" | while read line; do
+find . -type f -name "tst_*" -exec test -x {} \; -print | while read line; do
     echo "Running unit test: "
     echo "${line}"
     ($line)
